54267d18b04c2a9954e6af0d51925f0f924940d1
Alper Dogan <89078098+katonukushi@users.noreply.github.com>
2023-06-16 08:12:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const handleClick = () => {
+    setMessage("magie!");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Willkommen bei meiner individuellen React Anwendung!</h1>
+        <button onClick={handleClick}>Klicken Sie hier!</button>
+        <p>{message}</p>
       </header>
     </div>
   );
